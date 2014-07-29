40fee19034a74e45fcf25b5d0fb1361c57bbed56
ChrisjuhB <christian.broekmeulen@hotmail.com>
2014-07-29 11:48:41 +0200

    added untracked files

---
+++ src/Controller/FileController.php
@@ -0,0 +1,109 @@
+<?php
+  /**
+   * @file
+   * Contains \Drupal\file_entity\Controller\FileController.
+   */
+
+namespace Drupal\file_entity\Controller;
+
+/**
+ * Class FileController
+ */
+class FileController {
+
+  /**
+   * Manage files used on your site.
+   */
+  public function FileContent() {
+
+  }
+
+  /**
+   * List
+   */
+  public function FileContentList() {
+
+  }
+
+  /**
+   * general view, edit, delete for files
+   */
+  public function FileAdd() {
+
+  }
+
+  /**
+   * Upload
+   */
+  public function FileAddUpload() {
+
+  }
+
+  /**
+   * File
+   */
+  public function FileAddUploadFile() {
+
+  }
+
+  /**
+   * Archive
+   */
+  public function FileAddUploadArchive() {
+
+  }
+
+  /**
+   * File
+   *
+   * @param $file
+   */
+  public function File($file) {
+
+  }
+
+  /**
+   * View
+   *
+   * @param $file
+   */
+  public function FileView($file) {
+
+  }
+
+  /**
+   * Usage
+   *
+   * @param $file
+   */
+  public function FileUsage($file) {
+
+  }
+
+  /**
+   * Download
+   *
+   * @param $file
+   */
+  public function FileDownload($file) {
+
+  }
+
+  /**
+   * Edit
+   *
+   * @param $file
+   */
+  public function FileEdit($file) {
+
+  }
+
+  /**
+   * Delete
+   *
+   * @param $file
+   */
+  public function FileDelete($file) {
+
+  }
+}
---
+++ src/Controller/FileTypeController.php
@@ -0,0 +1,72 @@
+<?php
+  /**
+   * @file
+   * Contains \Drupal\file_entity\Controller\FileTypeController.
+   */
+
+namespace Drupal\file_entity\Controller;
+
+/**
+ * Class FileTypeController
+ */
+class FileTypeController {
+
+  /**
+   * Manage settings for the type of files used on your site.
+   */
+  public function FileTypesOverview() {
+
+  }
+
+  /**
+   * Add file type
+   */
+  public function FileTypesAdd() {
+
+  }
+
+  /**
+   * Manage settings for the type of files used on your site.
+   *
+   * @param $file_type
+   */
+  public function FileTypesManage($file_type) {
+
+  }
+
+  /**
+   * Enable
+   *
+   * @param $file_type
+   */
+  public function FileTypesManageEnable($file_type) {
+
+  }
+
+  /**
+   * Disable
+   *
+   * @param $file_type
+   */
+  public function FileTypesManageDisable($file_type) {
+
+  }
+
+  /**
+   * Revert
+   *
+   * @param $file_type
+   */
+  public function FileTypesManageRevert($file_type) {
+
+  }
+
+  /**
+   * Delete
+   *
+   * @param $file_type
+   */
+  public function FileTypesManageDelete($file_type) {
+
+  }
+}
