89f603115e50e48f21205666e034b5801a51b47b
Arild Matsson <arild@klavaro.se>
2014-07-31 10:52:09 +0200

    Updated access logic.

--- src/FileEntityAccessController.php
+++ src/FileEntityAccessController.php
@@ -7,13 +7,11 @@
 
 namespace Drupal\file_entity;
 
-use Drupal\Core\Entity\EntityControllerInterface;
-use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Language\LanguageInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\file\FileAccessController;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\file_entity\Entity\FileEntity;
 
 /**
  * Defines the access controller for the file entity type.
@@ -25,14 +23,8 @@ class FileEntityAccessController extends FileAccessController {
    */
   public function access(EntityInterface $entity, $operation, $langcode = LanguageInterface::LANGCODE_DEFAULT, AccountInterface $account = NULL) {
     $account = $this->prepareUser($account);
-
-    if ($account->hasPermission('bypass file access')) {
-      return TRUE;
-    }
-    if (!$account->hasPermission('access content')) {
-      return FALSE;
-    }
-    return parent::access($entity, $operation, $langcode, $account);
+    return $account->hasPermission('bypass file access') ||
+      parent::access($entity, $operation, $langcode, $account);
   }
 
   /**
@@ -40,22 +32,49 @@ class FileEntityAccessController extends FileAccessController {
    */
   public function createAccess($entity_bundle = NULL, AccountInterface $account = NULL, array $context = array()) {
     $account = $this->prepareUser($account);
-
-    if ($account->hasPermission('bypass file access')) {
-      return TRUE;
-    }
-    if (!$account->hasPermission('access content')) {
-      return FALSE;
-    }
-
-    return parent::createAccess($entity_bundle, $account, $context);
+    return $account->hasPermission('bypass file access') ||
+      parent::createAccess($entity_bundle, $account, $context);
   }
 
   /**
    * {@inheritdoc}
    */
   protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    return $account->hasPermission('create ' . $entity_bundle . ' content');
+    return $account->hasPermission('create files');
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, $langcode, AccountInterface $account) {
+    /** @var FileEntity $entity */
+
+    // Fall back to default behaviors on view.
+    if ($operation == 'view') {
+      $wrapper = file_entity_get_stream_wrapper(file_uri_scheme($entity->getFileUri()));
+
+      if (!empty($wrapper['private'])) {
+        // For private files, users can view private files if the
+        // user has the 'view private files' permission.
+        if ($account->hasPermission('view private files')) {
+          return TRUE;
+        }
+
+        // For private files, users can view their own private files if the user
+        // is not anonymous, and has the 'view own private files' permission.
+        if (!$account->isAnonymous() && $entity->getOwnerId() == $account->id() && $account->hasPermission('view own private files')) {
+          return TRUE;
+        }
+      }
+      elseif ($entity->isPermanent() && $entity->getOwnerId() == $account->id() && $account->hasPermission('view own files')) {
+        // For non-private files, allow to see if user owns the file.
+        return TRUE;
+      }
+      elseif ($entity->isPermanent() && $account->hasPermission('view files')) {
+        // For non-private files, users can view if they have the 'view files'
+        // permission.
+        return TRUE;
+      }
+    }
+  }
 }
