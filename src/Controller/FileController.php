8d7c0f5c8224176210bcf187c5b4b20b96eab781
Arild Matsson <arild@klavaro.se>
2014-08-04 16:26:50 +0200

    File download controller.

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -1,15 +1,21 @@
 <?php
-  /**
-   * @file
-   * Contains \Drupal\file_entity\Controller\FileController.
-   */
+/**
+ * @file
+ * Contains \Drupal\file_entity\Controller\FileController.
+ */
 
 namespace Drupal\file_entity\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\file\FileInterface;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class FileController
  */
-class FileController {
+class FileController extends ControllerBase {
 
   /**
    * Manage files used on your site.
@@ -22,13 +28,6 @@ class FileController {
    * List
    */
   public function FileContentList() {
-
-  }
-
-  /**
-   * general view, edit, delete for files
-   */
-  public function FileAdd() {
 
   }
 
@@ -54,24 +53,6 @@ class FileController {
   }
 
   /**
-   * File
-   *
-   * @param $file
-   */
-  public function File($file) {
-
-  }
-
-  /**
-   * View
-   *
-   * @param $file
-   */
-  public function FileView($file) {
-
-  }
-
-  /**
    * Usage
    *
    * @param $file
@@ -81,12 +62,44 @@ class FileController {
   }
 
   /**
-   * Download
+   * Returns a HTTP response for a file being downloaded.
    *
-   * @param $file
+   * @param FileInterface $file
+   *   The file to download, as an entity.
+   *
+   * @return Response
+   *   The file to download, as a response.
    */
-  public function FileDownload($file) {
+  public function download(FileInterface $file) {
+    // Ensure there is a valid token to download this file.
+    if (!($this->config('file_entity.settings')->get('allow_insecure_download'))) {
+      if (!isset($_GET['token']) || $_GET['token'] !== file_entity_get_download_token($file)) {
+        return new Response(t('Access to file @url denied', array('@url' => $file->getFileUri())), 403);
+      }
+    }
 
+    $headers = array(
+      'Content-Type' => mime_header_encode($file->getMimeType()),
+      'Content-Disposition' => 'attachment; filename="' . mime_header_encode(drupal_basename($file->getFileUri())) . '"',
+      'Content-Length' => $file->getSize(),
+      'Content-Transfer-Encoding' => 'binary',
+      'Pragma' => 'no-cache',
+      'Cache-Control' => 'must-revalidate, post-check=0, pre-check=0',
+      'Expires' => '0',
+    );
+
+    // Let other modules alter the download headers.
+    \Drupal::moduleHandler()->alter('file_download_headers', $headers, $file);
+
+    // Let other modules know the file is being downloaded.
+    \Drupal::moduleHandler()->invokeAll('file_transfer', array($file->getFileUri(), $headers));
+
+    try {
+      return new BinaryFileResponse($file->getFileUri(), 200, $headers);
+    }
+    catch (FileNotFoundException $e) {
+      return new Response(t('File @uri not found', array('@uri' =>$file->getFileUri())), 404);
+    }
   }
 
   /**
