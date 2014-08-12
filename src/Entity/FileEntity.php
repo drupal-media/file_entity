2f641f4ee80d31e00233f9706a9a7b70cebf8c47
Arild Matsson <arild@klavaro.se>
2014-08-12 11:11:52 +0200

    Revert "Renamed FieldDefinition to BaseFieldDefinition"
    This reverts commit 4b37e7bca42445f2d38798262883dbdbbf8c615c, in order to
    follow certain API changes in chronological order.

--- src/Entity/FileEntity.php
+++ src/Entity/FileEntity.php
@@ -9,7 +9,8 @@ namespace Drupal\file_entity\Entity;
 
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FieldDefinition;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\file\Entity\File;
 
 /**
@@ -95,7 +96,7 @@ class FileEntity extends File {
    */
   public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
     $fields = parent::baseFieldDefinitions($entity_type);
-    $fields['type'] = BaseFieldDefinition::create('entity_reference')
+    $fields['type'] = FieldDefinition::create('entity_reference')
       ->setLabel(t('File type'))
       ->setDescription(t('The type of the file.'))
       ->setSetting('target_type', 'file_type');
