515397861e62aac025eed9be21daeef97aeca561
Arild Matsson <arild@klavaro.se>
2014-09-12 17:14:15 +0200

    File view: hide h2 and try to display image

--- src/Entity/FileEntityViewBuilder.php
+++ src/Entity/FileEntityViewBuilder.php
@@ -7,7 +7,9 @@
 namespace Drupal\file_entity\Entity;
 
 use Drupal\Core\Entity\EntityViewBuilder;
-use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FormatterInterface;
+use Drupal\image\Entity\ImageStyle;
 
 /**
  * View builder for File Entity.
@@ -25,8 +27,29 @@ class FileEntityViewBuilder extends EntityViewBuilder {
       // Filename is already displayed as title.
       $build[$id]['filename']['#access'] = FALSE;
       $build[$id]['filesize'][0]['#markup'] = format_size($entity->getSize(), $langcode);
+
+      // Try to display a visual.
+      if ($entity->bundle() == 'image') {
+        // Approach described at http://drupal.stackexchange.com/a/115143
+        $definition = BaseFieldDefinition::create('image')
+          ->setName('image');
+        $items = \Drupal::typedDataManager()->create($definition, $entity->id(), $definition->getName(), $entity);
+        /** @var FormatterInterface $formatter */
+        $formatter = \Drupal::service('plugin.manager.field.formatter')->getInstance(array(
+          'field_definition' => $definition,
+          'view_mode' => 'default',
+          'configuration' => array(
+            'label' => 'hidden',
+            'type' => 'image',
+          ),
+        ));
+        // Set image style to 'large' or fallback to none.
+        $image_style = ImageStyle::load('large');
+        $formatter->setSetting('image_style', isset($image_style) ? $image_style->id() : '');
+        $formatter->prepareView(array($items));
+        $build[$id]['heyimage'] = $formatter->view($items);
+      }
     }
   }
-
 
 }
